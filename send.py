fb72c150eb646830749e806fa3dccdd9c92b40f7
Cullen <Cullenpuck@gmail.com>
2025-12-12 22:51:46 -0500

    final commit befoer I'm going to bed

--- send.py
+++ send.py
@@ -2,6 +2,7 @@
 #soruce file for the send functionality
 import os
 import socket
+import contact_search
 from contact_search import server
 
 
@@ -11,7 +12,10 @@ def send_file():
     choose file to send
     send file to port associated with that contact
     """
-
+    contact_search.list_online_contacts()
+    receiver_email = input("please enter the email you would like to send to: ").strip().lower()
+    while receiver_email not in contact_search.online_contacts.keys():
+        receiver_email = input("sorry that is not a recognized email of an online ")
 """
     #get list of online contacts
     #list_online_contacts()
@@ -22,4 +26,5 @@ def send_file():
         print("sorry that filename is invalid, enter a valid file name:")
     get socket of specified contact
     transmit file to receiver
-    """
+"""
+
